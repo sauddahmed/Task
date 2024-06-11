395e3f9ed5033cddccf473799a769605fdbbbf52
sauddahmed <111784648+sauddahmed@users.noreply.github.com>
2024-06-12 02:59:08 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getTasks, getTask, postTask, putTask, deleteTask } = require("../controllers/taskControllers");
-const { verifyAccessToken } = require("../middlewares.js");
+const { verifyAccessToken } = require("../middlewares");
 
 // Routes beginning with /api/tasks
 router.get("/", verifyAccessToken, getTasks);
