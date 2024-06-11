73b3eb3639566142716fc844109b8781b655fc1d
sauddahmed <111784648+sauddahmed@users.noreply.github.com>
2024-06-12 02:58:59 +0530

    Update profileRoutes.js

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getProfile } = require("../controllers/profileControllers");
-const { verifyAccessToken } = require("../middlewares.js");
+const { verifyAccessToken } = require("../middlewares");
 
 // Routes beginning with /api/profile
 router.get("/", verifyAccessToken, getProfile);
