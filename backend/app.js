0d1cee8bbf7f40d3d0170065dae2590a44844bc7
sauddahmed <111784648+sauddahmed@users.noreply.github.com>
2024-06-11 17:14:11 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+const result = dotenv.config({ path: "./file.env" });
+if (result.error) {
+  throw result.error;
+}
+
+console.log(result.parsed); // Debugging line to check loaded environment variables
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+
+app.use(express.json());
+app.use(cors());
+
+const mongoUrl = process.env.MONGODB_URL;
+console.log("MongoDB URL:", mongoUrl); // Debugging line to check if the URL is read correctly
+
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongodb connected..."))
+  .catch((err) => console.error("Mongodb connection error:", err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/profile", profileRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/public")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"))
+  );
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
+});
