c87d9ed704d20af71a844a4a16768d726187a3be
sauddahmed <111784648+sauddahmed@users.noreply.github.com>
2024-06-12 03:39:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 // Load environment variables from .env file
-const result = dotenv.config({ path: "./file.env" });
-if (result.error) {
-  throw result.error;
-}
+dotenv.config(); // This will look for a .env file in the root of your project
 
-console.log(result.parsed); // Debugging line to check loaded environment variables
+console.log(process.env); // Debugging line to check loaded environment variables
 
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
@@ -21,25 +18,34 @@ app.use(express.json());
 app.use(cors());
 
 const mongoUrl = process.env.MONGODB_URL;
-console.log("MongoDB URL:", mongoUrl); // Debugging line to check if the URL is read correctly
+if (!mongoUrl) {
+  console.error("MongoDB URL is not defined");
+  process.exit(1);
+}
 
 mongoose
   .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongodb connected..."))
-  .catch((err) => console.error("Mongodb connection error:", err));
+  .catch((err) => {
+    console.error("Mongodb connection error:", err);
+    process.exit(1);
+  });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.use(express.static(path.resolve(__dirname, "../frontend/public")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"))
   );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
 
-// Use the PORT environment variable provided by Render
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
