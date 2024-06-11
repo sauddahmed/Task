691e074d27203594cea7d18a47ad830c98327074
sauddahmed <111784648+sauddahmed@users.noreply.github.com>
2024-06-12 04:03:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -35,17 +35,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-  );
-} else {
-  app.use(express.static(path.resolve(__dirname, "../frontend/public")));
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
+app.use(express.static(path.resolve(__dirname, "../frontend/public")));
+
+// Serve the index.html file for any other routes
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
